2ed3d121534ef33227ee63234130dad732d4eddf
Antoine Guéneau <a.gueneau@hegyd.fr>
2020-12-18 12:58:43 +0100

    Hello world with template

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 # coding: UTF-8
 from django.urls import path
-app_name = 'users'
-urlpatterns = [
+from users import views
 
+app_name = 'users'
+
+urlpatterns = [
+    path('hello/', views.hello, name="hello"),
 ]
